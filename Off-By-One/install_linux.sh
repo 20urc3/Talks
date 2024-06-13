03fb03a630151bec7699753294d3570e2569da15
20urc3 <94982366+20urc3@users.noreply.github.com>
2024-06-13 19:15:18 +0200

    Update install_linux.sh

--- Off-By-One/install_linux.sh
+++ Off-By-One/install_linux.sh
@@ -30,3 +30,6 @@ cd ..
 wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
+git clone https://github.com/google/syzkaller
+cd syzkaller
+make
