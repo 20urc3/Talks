63904a1d06bf2c629825ea061aaa9a1349b5ebe4
20urc3 <94982366+20urc3@users.noreply.github.com>
2024-06-13 18:58:16 +0200

    Update install_linux.sh

--- Off-By-One/install_linux.sh
+++ Off-By-One/install_linux.sh
@@ -1,5 +1,7 @@
 apt-get update -y
-# Installing dependencies for AFL++
+###################################
+# Installing AFL++
+# Dependencies
 apt-get install -y build-essential python3-dev automake cmake git flex bison libglib2.0-dev libpixman-1-dev python3-setuptools python3-pip ninja-build 
 pip install unicorn
 
@@ -14,3 +16,12 @@ git clone https://github.com/AFLplusplus/AFLplusplus
 cd AFLplusplus
 make
 sudo make install
+cd ..
+###################################
+# Install fuzzili
+wget https://github.com/googleprojectzero/fuzzilli/archive/refs/tags/v0.9.3.tar.gz
+tar -xf v0.9.3.tar.gz
+cd v0.9.3.tar.gz
+swift build -c release -Xlinker='-lrt'
+cd ..
+###################################
